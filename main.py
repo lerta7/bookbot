e62178536a590f985ba002d51db1a1ca54df00cf
Your Name <you@example.com>
2025-01-03 21:32:46 +0200

    commit

--- main.py
+++ main.py
@@ -6,4 +6,7 @@ with open(BOOK_PATH) as f:
 def count_words(word_str):
     return len(word_str.split())
 
+def count_chars(word_str):
+    lower_str = word_str.lowercase()
+
 print(count_words(file_contents))
