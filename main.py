34281886850e3879ea65e5288240ed2a3fe71ba4
Your Name <you@example.com>
2025-01-03 22:19:10 +0200

    L12

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 BOOK_PATH = "books//frankenstein.txt"
 
-with open(BOOK_PATH) as f:
-    file_contents = f.read()
 
 def count_words(word_str):
     return len(word_str.split())
 
-def count_chars(word_str):
-    lower_str = word_str.lowercase()
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered.isalpha():
+            if lowered in chars:
+                chars[lowered] += 1
+            else:
+                chars[lowered] = 1
+    sorted_chars = sorted(chars.items(), key=lambda x:(x[1], x[0]), reverse=1)
+    sorted_chars = dict(sorted_chars)
+    return sorted_chars
 
-print(count_words(file_contents))
+def main():
+    with open(BOOK_PATH) as f:
+        file_contents = f.read()
+
+    chars = get_chars_dict(file_contents)
+    word_count = count_words(file_contents)
+
+    # print start of report
+    report_text = f"""--- Begin report of {BOOK_PATH} ---
+    {word_count} words found in the document"""
+    print(report_text)
+
+    #  print info for each char
+    for char in chars:
+        print(f"The '{char}' character was found {chars[char]} times")
+    print("--- End report ---")
+
+main()
